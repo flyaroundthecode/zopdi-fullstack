dcd3ad35cf285d613dd86089b0203c413db38cfd
flyaroundthecode <flyaroundthecode@outlook.com>
2016-04-04 23:40:22 +0530

    Initial commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('zopdiFullstackApp')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
